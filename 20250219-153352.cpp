627ea2581694161e2266d3d8c7a3b601ee4ce0e0
millicent-vutagwa <d33-2459-2023@student.mksu.ac.ke>
2025-02-24 20:11:32 +0300

    Add files via upload

---
+++ 20250219-153352.cpp
@@ -0,0 +1,22 @@
+/*
+Author: Millicent 
+Reg No:D33-2459-2023
+program to find area of a rectangle 
+Date:18.2.2025
+*/
+# include <stdio.h> //print(),scanf
+int main(){
+variable declaration and initialisation; 
+int a ;
+int b ;
+int area;
+printf("program to find area of a rectangle\n");
+printf("Enter the length:\n");
+scanf("%d",&a);
+printf("Enter the width:\n");
+scanf("%d",&a);
+area=a×b;
+printf("The area is %d",area);
+return 0;// execution successful 
+};
+{;
