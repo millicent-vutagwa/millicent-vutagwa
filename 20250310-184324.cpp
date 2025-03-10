7437639f1d78028f17ff994e7ef79672a7bdb6ef
millicent-vutagwa <d33-2459-2023@student.mksu.ac.ke>
2025-03-10 18:59:08 +0300

    Add files via upload

---
+++ 20250310-184324.cpp
@@ -0,0 +1,20 @@
+/*
+do-while loop
+Author: Millicent 
+Reg No:D33-2459-2023
+date:9.3.2025
+*/
+#include <stdio.h>
+//main function 
+int main(){
+
+int i=1;//start 
+
+do{
+printf("number is:%d and cube of %d is:%d\n",i,i,i*i*i);
+i++;//step
+
+}while (i<=100);//stop
+return 0;
+
+}
