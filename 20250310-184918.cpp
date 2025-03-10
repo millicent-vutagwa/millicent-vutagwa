ae71428229135bb49653e5a58b6d7d76f8bc82b3
millicent-vutagwa <d33-2459-2023@student.mksu.ac.ke>
2025-03-10 18:56:32 +0300

    Add files via upload

---
+++ 20250310-184918.cpp
@@ -0,0 +1,20 @@
+/*
+for loop
+Author: Millicent 
+Reg No:D33-2459-2023
+date:9.3.2025
+*/
+#include <stdio.h>
+//main function 
+int main(){
+
+int i=1;
+
+for(i=1; i<=100; i++){
+
+printf("number is:%d and cube of %d is:%d\n",i,i,i*i*i);
+}
+
+return 0;
+
+}
